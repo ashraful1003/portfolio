78a9c62d5dfd5ddfed355c6226ba7e5abb4f59a2
Ashraful Alam <alamashraful1003@gmail.com>
2022-01-15 23:10:37 +0600

    complete

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,2 +1,22 @@
-var w = window.innerWidth;
-document.write(w);
+function myFunction() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var division = document.getElementById("division").value;
+    var subject = document.getElementById("subject").value;
+
+    if (name == "" || name == null) {
+        alert("Enter Name");
+    }
+    else if (email == "" || email == null) {
+        alert("Enter Email");
+    }
+    else if (division == "--Select Division--") {
+        alert("Enter Division");
+    }
+    else if (subject == "" || subject == null) {
+        alert("Enter Something!");
+    }
+    else {
+        alert("Hello, " + name + "\nwith Email:" + email + "\nFrom: " + division + "\nYour Message:\n" + subject + "\n\nYour Response is SUCCESSFUL! Thank You for Being with ME!");
+    }
+}
